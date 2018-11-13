8c6f298bedcdf15c7ba1df561ba707a68f7a1356
skfro266 <skfro266@naver.com>
2018-11-13 11:49:54 +0900

    upload

---
+++ php/include/top.php
@@ -0,0 +1,13 @@
+<div class="top_menu clear">
+    <ul>
+        <li>Sitemap</li>
+        <li>Join</li>
+        <li>Login</li>
+    </ul>
+</div>
+<div class="top">
+    <div class="logo">LOGO</div>
+    <div class="bars">
+        <span class="fa fa-bars"></span>
+    </div>
+</div>
---
+++ php/index.php
@@ -0,0 +1,7 @@
+<? include_once "include/head.php"; ?>
+<? include_once "include/top.php"; ?>
+<? include_once "include/nav.php"; ?>
+Hellow World~
+<? include_once "include/bottom.php"; ?>
+<? include_once "include/foot.php"; ?>
+
